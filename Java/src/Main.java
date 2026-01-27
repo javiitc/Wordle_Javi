a2cd1087f55dfc744e841a2f6616ae17ea0400b3
Javi <itsjavii1610@gmail.com>
2026-01-27 11:59:47 +0100

    solo Dios sabe como acabará esto

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,2 +1,10 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        
+    }
 }
