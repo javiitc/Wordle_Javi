01996b2983bbdd65b9b7cb2db2f78cf305c17d00
Javi <itsjavii1610@gmail.com>
2026-01-27 12:37:47 +0100

    No se que he hecho pero parece pro

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,6 +10,27 @@ public class Main {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
-        
+
+        try {
+            URL url = new URL ("https://random-word-api.herokuapp.com/word?lang=es&length=5");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String respuesta = br.readLine();
+            br.close();
+
+            String palabra = respuesta
+                    .replace("[", "")
+                    .replace("]", "")
+                    .replace("\"", "");
+
+            System.out.println(palabra);
+
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
